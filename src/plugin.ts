c03fd018be56214a72b1caef5e9a8b5530123d9a
rgoldfinger-quizlet <roger.goldfinger@quizlet.com>
2019-04-05 10:12:50 -0700

    fixes

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,18 +1,24 @@
 export function buildPlugin(visitors: Function[]) {
-    const visitorMap: { [name: string]: Function } = {};
-    for (const visitor of visitors) visitorMap[visitor.name] = visitor;
+  const visitorMap: { [name: string]: Function } = {};
+  for (const visitor of visitors)
+    visitorMap[visitor.name] = path => {
+      const comments = path && path.node.comments;
+      visitor(path);
+      if (!path.node.comments) {
+        path.node.comments = comments;
+      }
+    };
 
-    return () => ({
-        name: 'babel-plugin-flow-to-typescript',
-        visitor: visitorMap,
+  return () => ({
+    name: "babel-plugin-flow-to-typescript",
+    visitor: visitorMap,
 
-        //tslint:disable:no-any
-        manipulateOptions(_opts: any, parserOpts: any) {
-            parserOpts.plugins.push('flow');
-            parserOpts.plugins.push('jsx');
-            parserOpts.plugins.push('classProperties');
-            parserOpts.plugins.push('objectRestSpread');
-
-        }
-    });
+    //tslint:disable:no-any
+    manipulateOptions(_opts: any, parserOpts: any) {
+      parserOpts.plugins.push("flow");
+      parserOpts.plugins.push("jsx");
+      parserOpts.plugins.push("classProperties");
+      parserOpts.plugins.push("objectRestSpread");
+    }
+  });
 }
