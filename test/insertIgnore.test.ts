259cdbd979028ad884717615601bc8831f4b2219
rgoldfinger-quizlet <roger.goldfinger@quizlet.com>
2019-06-13 16:01:26 -0700

    cleanup and fix tests

--- test/insertIgnore.test.ts
+++ test/insertIgnore.test.ts
@@ -20,7 +20,23 @@ describe("insertIgnore", () => {
     const testCode = fs.readFileSync(fixturePath, "utf8");
     let codeSplitByLine = testCode.split("\n");
     diagnostics.forEach((diagnostic, _errorIndex) => {
-      codeSplitByLine = insertIgnore(diagnostic, codeSplitByLine);
+      codeSplitByLine = insertIgnore(diagnostic, codeSplitByLine, false);
+    });
+    expect(codeSplitByLine.join("\n")).toMatchSnapshot();
+  });
+  it("should insert into jsx when the option is passed", () => {
+    const fixturePath = path.join(__dirname, "./insertIgnore.fixture.tsx");
+
+    const program = ts.createProgram([fixturePath], compilerOptions.options);
+
+    const diagnostics = ts
+      .getPreEmitDiagnostics(program)
+      .filter(d => !!d.file)
+      .reverse();
+    const testCode = fs.readFileSync(fixturePath, "utf8");
+    let codeSplitByLine = testCode.split("\n");
+    diagnostics.forEach((diagnostic, _errorIndex) => {
+      codeSplitByLine = insertIgnore(diagnostic, codeSplitByLine, true);
     });
     expect(codeSplitByLine.join("\n")).toMatchSnapshot();
   });
